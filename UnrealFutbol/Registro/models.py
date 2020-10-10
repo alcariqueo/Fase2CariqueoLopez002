81304150e296087ab224ac2b22570379ac5a7689
unknown <al.cariqueo@alumnos.duoc.cl>
2020-10-10 20:05:35 -0300

    Creacion de modelos

--- UnrealFutbol/Registro/models.py
+++ UnrealFutbol/Registro/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class Usuario(models.Model):
+    Nombre=models.CharField(max_length=100)
+    Apellido=models.CharField(max_length=100)
+    Email=models.EmailField( null=True, blank=True)
+    FechaN=models.DateField(null=True, blank=True)
+    Telefono=models.IntegerField()
+    password=models.CharField(max_length=6)
+    passwordC=models.CharField(max_length=6)
+
+
+    def str(self):
+        return "usuario creado"
+        
+
+   
