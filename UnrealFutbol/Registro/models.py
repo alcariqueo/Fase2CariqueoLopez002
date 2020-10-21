f0a3373852944327dde91fd3c481cbd19cc5a3a0
unknown <al.cariqueo@alumnos.duoc.cl>
2020-10-21 10:25:30 -0300

    Unreal Futbol: Creando Página de inicio!

--- UnrealFutbol/Registro/models.py
+++ UnrealFutbol/Registro/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-
+from django.urls import reverse
 import uuid
 
 
 class Usuario(models.Model):
-    Nombre=models.CharField(max_length=100)
-    Apellido=models.CharField(max_length=100)
-    Email=models.EmailField( null=True, blank=True)
+    Nombre=models.CharField(max_length=50)
+    Apellido=models.CharField(max_length=50)
+    Email=models.EmailField(null=True, blank=True)
     FechaN=models.DateField(null=True, blank=True)
-    Telefono=models.IntegerField()
+    Telefono=models.IntegerField(null=True, blank=True)
     password=models.CharField(max_length=6)
     passwordC=models.CharField(max_length=6)
 
 
     def str(self):
-        return "usuario creado"
-        
+        return self.Email
 
    
